8d6b5bf22060205c0c52e0157042185723c15a25
Bandi Sai Chandu <Bandi Sai Chandu@DESKTOP-FONC2FO>
2025-04-02 23:42:48 +0530

    First commi

--- src/revature/Bubbl.java
+++ src/revature/Bubbl.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Bubbl 
 {
    public static void main(String[] args) {
+
 	  int[] a= {8,9,3,6,2,9,0};
 	  for (int i = 0; i < a.length; i++) {
 		for (int j = 0; j < a.length-1; j++) 
@@ -18,5 +19,6 @@ public class Bubbl
 		}
 	  }
 	  System.out.println(Arrays.toString(a));
+	  System.out.println("hi iam a aman");
    }
 }
